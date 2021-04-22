f5afc94bb7db3c57ef8d741646290e2ba6efcfff
maa123 <maa123.lq@gmail.com>
2021-04-22 14:12:00 +0900

    boa to v8

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
 use serde::{Serialize, Deserialize};
-use boa::{exec::Executable, parse, Context};
 
-fn exec(src: String) -> Result<String, String> {
-    let mut context = Context::new();
-    let expr = match parse(src, false) {
-        Ok(res) => res,
-        Err(e) => {
-            return Err(format!(
-                "Parse Uncaught {}",
-                context
-                    .throw_syntax_error(e.to_string())
-                    .expect_err("interpreter.throw_syntax_error() did not return an error")
-                    .display()
-            )
-            .into());
+fn get_error(scope: &mut rusty_v8::TryCatch<rusty_v8::HandleScope>) -> String {
+    if let Some(exp) = scope.exception() {
+        rusty_v8::Exception::create_message(scope, exp).get(scope).to_rust_string_lossy(scope)
+    } else {
+        "".to_string()
+    }
+}
+
+fn exec_v8(input: &str) -> Result<String, String> {
+    let mut isolate = rusty_v8::Isolate::new(Default::default());
+    let base_scope = &mut rusty_v8::HandleScope::new(&mut isolate);
+    let context = rusty_v8::Context::new(base_scope);
+    let context_scope = &mut rusty_v8::ContextScope::new(base_scope, context);
+    let scope = &mut rusty_v8::TryCatch::new(context_scope);
+    let code = rusty_v8::String::new(scope, &input).unwrap();
+    code.to_rust_string_lossy(scope);
+    if let Some(script) = rusty_v8::Script::compile(scope, code, None) {
+        if let Some(val) = script.run(scope) {
+            if let Some(result) = val.to_string(scope) {
+                Ok(result.to_rust_string_lossy(scope))
+            } else {
+                Err(get_error(scope))
+            }            
+        } else {
+            Err(get_error(scope))
         }
-    };
-    expr.run(&mut context)
-        .map_err(|e| format!("Uncaught {}", e.display()))
-        .map(|v| v.display().to_string())
+    } else {
+        Err(get_error(scope))
+    }
 }
 
 #[derive(Serialize)]
@@ -32,6 +42,10 @@ struct Input {
 }
 
 fn main() {
+    let platform = rusty_v8::new_default_platform().unwrap();
+    rusty_v8::V8::initialize_platform(platform);
+    rusty_v8::V8::initialize();
+    
     let mut input_str = String::new();
     if let Err(_) = std::io::stdin().read_line(&mut input_str) {
         let result = serde_json::to_string(&ScriptResult {
@@ -41,15 +55,15 @@ fn main() {
         print!("{}", result)
     }
     let input: Input = serde_json::from_str(&input_str).unwrap();
-    let str = input.script;
-    let res = match exec(str.to_string()){
+    let script = input.script;
+    let res = match exec_v8(&script) {
         Ok(s) => ScriptResult {
             result: s,
             error: "".to_string()
         },
-        Err(e) => ScriptResult {
+        Err(s) => ScriptResult {
             result: "".to_string(),
-            error: e
+            error: s
         }
     };
     let result = serde_json::to_string(&res).unwrap();
